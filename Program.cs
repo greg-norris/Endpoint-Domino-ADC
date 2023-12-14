b9d64b570feae8df76161fe9909c8d3330c7a00c
greg-norris <greg.norris@ghielectronics.com>
2023-12-14 12:39:50 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using GHIElectronics.Endpoint.Devices.Adc;
+using static GHIElectronics.Endpoint.Core.EPM815;
+
+
+var adcController = new AdcController(Adc.Pin.PA6);
+
+
+while (true)
+{
+    var v = adcController.Read();
+
+    Console.WriteLine(v.ToString());
+    Thread.Sleep(10);
+
+}
